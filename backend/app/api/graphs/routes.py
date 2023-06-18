56ec1d6d7b383d33ec8f970789d1bb92f2811217
Piotr Dros <piotr.dros01@gmail.com>
2023-06-18 16:37:46 +0200

    :sparkles: React App -> Next.js

--- backend/app/api/graphs/routes.py
+++ backend/app/api/graphs/routes.py
@@ -1,7 +1,7 @@
 from app.api.graphs import bp
 from app.services.graphs import graph1,graph2,graph3,graph4,graph5
 
-@bp.route('/graph1')
+@bp.route('/1')
 def index():
     fig = graph1()
     
@@ -9,7 +9,7 @@ def index():
 
     return fig_json
 
-@bp.route('/graph2')
+@bp.route('/2')
 def index2():
     fig = graph2()
     
@@ -17,7 +17,7 @@ def index2():
 
     return fig_json
 
-@bp.route('/graph3')
+@bp.route('/3')
 def index3():
     fig = graph3()
     
@@ -25,7 +25,7 @@ def index3():
 
     return fig_json
 
-@bp.route('/graph4')
+@bp.route('/4')
 def index4():
     fig = graph4()
     
@@ -33,7 +33,7 @@ def index4():
 
     return fig_json
 
-@bp.route('/graph5')
+@bp.route('/5')
 def index5():
     fig = graph5()
     
---
+++ frontend/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({ subsets: ['latin'], weight: '400' });
+
+export const metadata = {
+  title: 'Currency data visualisation',
+  description: 'Visualisation project',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}
