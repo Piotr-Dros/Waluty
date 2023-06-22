d8320e7ab2818c982999abaa56a7fba0c4f3fca4
Piotr Dros <piotr.dros01@gmail.com>
2023-06-23 00:26:12 +0200

    :construction: Trying to deploy backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,20 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 
-from config import Config
+app = Flask(__name__)
+CORS(app)
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    CORS(app)
-    
-    app.config.from_object(config_class)
 
-    # For testing blueprint
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+# For testing blueprint
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
 
-    # Blueprint here
-    from app.api.graphs import bp as graph_bp
-    app.register_blueprint(graph_bp, url_prefix='/api/graphs')
+# Blueprint here
+from app.api.graphs import bp as graph_bp
+app.register_blueprint(graph_bp, url_prefix='/api/graphs')
 
-    return app
